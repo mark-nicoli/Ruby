8b9a92da94bb8d5d287791a11a2e9d243d5cd2cc
Mark Nicoli <mark21198@gmail.com>
2019-06-04 15:23:36 +0100

    Add files via upload

---
+++ Learning_ruby/ident.rb
@@ -0,0 +1,7 @@
+puts "enter a number"
+num1 = gets.to_f    #to_f means change it to a floating point
+puts "enter another number"
+num2 = gets.to_f
+
+puts "added: "+(num1+num2).to_s
+puts "subtracted: "+(num1-num2).to_s
