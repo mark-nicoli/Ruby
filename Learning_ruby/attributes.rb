a4ca4c0e0fbc3f3470693381f518bcff72b1caa5
mark-nicoli <mark21198@gmail.com>
2019-06-10 14:02:12 +0100

    added attributes.rb

---
+++ Learning_ruby/attributes.rb
@@ -0,0 +1,42 @@
+class Point
+  def initialize(x,y)
+    @x,@y = x,y
+  end
+
+  #define reader methods (getters)
+  attr_reader:x  #allows .x access
+  attr_reader:y
+
+  #define writer method (setters)
+  attr_writer:x   #allows x=? access
+  attr_writer:y
+
+  def to_s
+    "(#{@x},#{@y})"
+  end
+end
+
+point = Point.new(4,-7)
+puts "point is (#{point.to_s})"
+point.x = -2
+point.y = 6
+puts "point is (#{point.x},#{point.y})"
+
+class ColorPoint
+  def initialize(x,y,color)
+    @x,@y = x,y
+    @color = color
+  end
+
+  attr_accessor:color
+  #make x and y read only
+  attr_reader:x
+  attr_reader:y
+
+end
+
+cp = ColorPoint.new(3,5,"red")
+puts "color point (#{cp.x},#{cp.y}) is (#{cp.color})"
+
+cp.color = "blue"
+puts "color point (#{cp.x},#{cp.y}) is (#{cp.color})"
