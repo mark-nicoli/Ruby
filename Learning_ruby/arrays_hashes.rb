a7a475898d596fdbdb3ee7dceaffebed95d9aa72
mark-nicoli <mark21198@gmail.com>
2019-06-11 14:17:54 +0100

    add hashes and arrays code

---
+++ Learning_ruby/arrays_hashes.rb
@@ -0,0 +1,12 @@
+#ruby arrays
+array = ["mark", 10, 13, "random string", "whaterver"]
+array.each do |i|
+  puts i
+end
+
+#ruby hashes
+hash = colors = { "red" => 0xf00, "green" => 0x0f0, "blue" => 0x00f }
+hash.each do |key, value|
+  puts key "is ",value, "/n"
+end
+ 
